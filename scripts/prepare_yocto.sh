0bac1202e2b2ba70f075b051c995c53e1b1c0203
mirrin00 <mirrin1905@outlook.com>
2023-05-31 14:13:09 +0300

    Fixed scripts, updated wiki

--- scripts/prepare_yocto.sh
+++ scripts/prepare_yocto.sh
@@ -1,28 +1,37 @@
 #/bin/bash
 
 set -x
+set -e
 
 ROOT_PROJECT_PATH=$(pwd)
+YOCTO_VER_NAME="langdale"
 LAYER_NAME="meta-ntb-cxl"
-if [ ! -d build_vm ]; then
-    mkdir build_vm
+if [ $# -lt 1 ]; then
+    echo -e "\e[1;33mBuild dir is not provided, using 'build_vm' dir\e[0m"
+    YOCTO_WORK_DIR="build_vm"
+else
+    YOCTO_WORK_DIR="$1"
 fi
-cd build_vm
+if [ ! -d $YOCTO_WORK_DIR ]; then
+    mkdir $YOCTO_WORK_DIR
+fi
+cd $YOCTO_WORK_DIR
 if [ ! -d poky ]; then
     git clone git://git.yoctoproject.org/poky.git
 fi
 cd poky
-if ! git checkout my-kirkstone; then
-    git checkout -t origin/kirkstone  -b my-kirkstone
+if ! git checkout my-$YOCTO_VER_NAME; then
+    git checkout -t origin/$YOCTO_VER_NAME  -b my-$YOCTO_VER_NAME
 fi
 git pull
 
-source oe-init-build-env
-if [ -z $(bitbake-layers show-layers | grep $LAYER_NAME) ]; then
+FULL_LAYER_NAME="$(pwd)/$LAYER_NAME"
+source oe-init-build-env build
+if [ -z "$(bitbake-layers show-layers | grep $LAYER_NAME)" ]; then
     bitbake-layers create-layer ../$LAYER_NAME
+    echo -e "BBLAYERS += \"$FULL_LAYER_NAME\"" >> ./conf/bblayers.conf
 fi
-cp $ROOT_PROJECT_PATH/configs/yocto/local.conf ./conf/local.conf
-cp $ROOT_PROJECT_PATH/yocto_files/bblayers.conf ./conf/bblayers.conf
+cp $ROOT_PROJECT_PATH/yocto_files/configs/local.conf ./conf/local.conf
 cp -r $ROOT_PROJECT_PATH/yocto_files/recipes-kernel ../$LAYER_NAME/recipes-kernel
 
 set +x
