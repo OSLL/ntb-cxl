e48e24c8ba485721b662f3506fefb8266dcf6a5f
AndrewGavril <gavrilov.andrew1999@yandex.ru>
2023-06-15 15:20:58 +0300

    Fixed build paths and docker run

--- run_vm_build.sh
+++ run_vm_build.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 PATH=$(pwd)
 BUILD_FOLDER_NAME=build_vm_image/
+
+
 BUILD_PATH=$PATH/$BUILD_FOLDER_NAME
 
-docker build . --build-arg build_folder_name=$BUILD_FOLDER_NAME -t yocto_build
-
-docker run --network host yocto_build -v $BUILD_PATH:/build_vm_image/
+/usr/bin/docker build . --build-arg build_folder_name=$BUILD_FOLDER_NAME -t yocto_build
+/usr/bin/docker run --network host -v ${BUILD_PATH}:/project/$BUILD_FOLDER_NAME yocto_build
