239a8b6d0a77aa630fd89448228d4d06dafbb34f
Maxim Karasev <mxkrsv@disroot.org>
2023-07-13 12:05:07 +0300

    scripts: Add qemu_{enter,leave}_devenv.sh for quick development

---
+++ scripts/qemu_enter_devenv.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo "Entering development environment..."
+
+set -x
+set -e
+
+. "$(dirname $0)"/common_variables.sh
+
+cd "$YOCTO_WORK_DIR"/poky
+
+source oe-init-build-env build
+
+devtool modify qemu-native || true
+
+set +x
+
+cat <<EOF
+
+======================================================================
+Your working directory is at poky/build/workspace/sources/qemu-native.
+Make your changes, commit them and run qemu_leave_devenv.sh.
+======================================================================
+
+EOF
---
+++ scripts/qemu_leave_devenv.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+echo "Applying changes..."
+
+set -x
+set -e
+
+. "$(dirname $0)"/common_variables.sh
+
+cd "$YOCTO_WORK_DIR"/poky
+
+source oe-init-build-env build
+
+devtool finish qemu-native ../meta-ntb-cxl
+
+DESTDIR="$ROOT_PROJECT_PATH"/yocto_files/recipes-devtools/qemu
+mkdir -p "$DESTDIR"
+cd ../meta-ntb-cxl/recipes-devtools/qemu
+
+# obsolete patches will remain here, remove them
+rm -r "$DESTDIR"/qemu-native
+
+cp -r qemu-native "$DESTDIR"
+rm "$DESTDIR"/qemu-native/cross.patch # why is it there anyway?
+cp qemu-native_%.bbappend "$DESTDIR"
+
+set +x
+
+echo "Finished applying changes"
