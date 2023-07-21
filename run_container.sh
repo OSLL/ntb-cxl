941d9da1072e8725d200688b3a174ae06835f2bd
Maxim Karasev <mxkrsv@disroot.org>
2023-07-21 13:59:42 +0300

    run_container.sh: mount more directories required for making changes in root repository tree

--- run_container.sh
+++ run_container.sh
@@ -21,4 +21,8 @@ BUILD_PATH="$PWD"/"$BUILD_FOLDER_NAME"
 
 mkdir -p "$BUILD_FOLDER_NAME"
 docker build . --build-arg build_folder_name="$BUILD_FOLDER_NAME" --build-arg user_id="$(id -u)" -t yocto
-docker run --rm --network host -v "$BUILD_PATH":/home/user/project/"$BUILD_FOLDER_NAME" yocto "$CMD"
+docker run --rm --network host \
+	-v "$BUILD_PATH":/home/user/project/"$BUILD_FOLDER_NAME" \
+	-v "$PWD"/qemu_src:/home/user/project/qemu_src \
+	-v "$PWD"/yocto_files:/home/user/project/yocto_files \
+	yocto "$CMD"
