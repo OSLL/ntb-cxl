69d12f2612bfc5ceadddcad7a5f10d788a2f41af
Maxim Karasev <mxkrsv@disroot.org>
2023-07-21 13:04:56 +0300

    scripts/qemu_enter_devenv.sh: add workaround for 'git am is in progress'

--- scripts/qemu_enter_devenv.sh
+++ scripts/qemu_enter_devenv.sh
@@ -13,6 +13,16 @@ source oe-init-build-env build
 
 devtool modify --no-overrides qemu-system-native
 
+# Sort of a weird hack, don't have any ideas about why things are like that
+cd workspace/sources/qemu-system-native
+git config --local user.name a
+git config --local user.email a
+{
+	rm -r .git/rebase-apply
+	git am
+} || true
+cd -
+
 set +x
 
 cat <<EOF
