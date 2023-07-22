8dcc5dc0c77c34a5737a1b68a91552cdbf0b101a
Maxim Karasev <mxkrsv@disroot.org>
2023-07-22 18:50:41 +0300

    scripts/build_yocto.sh: add 'bitbake qemu-system-native' since qemu is only built once

--- scripts/build_yocto.sh
+++ scripts/build_yocto.sh
@@ -14,6 +14,7 @@ source oe-init-build-env build
 bitbake -c kernel_configme -f virtual/kernel && \
 bitbake -c compile -f virtual/kernel && \
 bitbake -c deploy virtual/kernel && \
+bitbake qemu-system-native && \
 bitbake core-image-full-cmdline
 
 set +x
