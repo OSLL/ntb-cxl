bea788e89663ca5e863466abf6af0e2c3592e82c
Maxim Karasev <mxkrsv@disroot.org>
2023-08-01 00:17:23 +0300

    scripts/run_vms.sh: introduce cmdline logic to fix handling of master

--- scripts/run_vms.sh
+++ scripts/run_vms.sh
@@ -30,11 +30,19 @@ else
 	IVSHMEM_COMMON_OPTIONS="$IVSHMEM_COMMON_OPTIONS_DEFAULT $IVSHMEM_COMMON_OPTIONS"
 fi
 
+CMDLINE_COMMON_DEFAULT="root=/dev/vda rw ip=dhcp oprofile.timer=1 tsc=reliable no_timer_check rcupdate.rcu_expedited=1"
+
+if [ "$CMDLINE_COMMON_OVERRIDE" ]; then
+	CMDLINE_COMMON="$CMDLINE_COMMON_OVERRIDE"
+else
+	CMDLINE_COMMON="$CMDLINE_COMMON_DEFAULT $CMDLINE_COMMON"
+fi
+
 COMMON_OPTIONS_DEFAULT="-usb -device usb-tablet -usb -device usb-kbd \
 	-cpu IvyBridge -machine q35,i8042=off -smp 4 -m 256 \
-	-nographic -monitor null \
-	-append 'root=/dev/vda rw  ip=dhcp oprofile.timer=1 tsc=reliable no_timer_check rcupdate.rcu_expedited=1'"
-VM1_OPTIONS_DEFAULT="-serial telnet::8000,server,nowait -append ivshmem_master"
+	-nographic -monitor null"
+
+VM1_OPTIONS_DEFAULT="-serial telnet::8000,server,nowait"
 VM2_OPTIONS_DEFAULT="-serial telnet::8001,server,nowait"
 
 if [ "$COMMON_OPTIONS_OVERRIDE" ]; then
@@ -60,12 +68,14 @@ fi
 "$qemu" \
 	-drive file="$drive",if=virtio,format=raw \
 	-kernel "$kernel" \
+	-append "$CMDLINE_COMMON ivshmem_master" \
 	$COMMON_OPTIONS \
 	$VM1_OPTIONS &
 
 "$qemu" \
 	-drive file="$drive2",if=virtio,format=raw \
 	-kernel "$kernel2" \
+	-append "$CMDLINE_COMMON" \
 	$COMMON_OPTIONS \
 	$VM2_OPTIONS &
 
