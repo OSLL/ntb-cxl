37c0400e5b8a17a034a02bc3e004176c15087f81
Maxim Karasev <mxkrsv@disroot.org>
2023-08-01 00:15:26 +0300

    scripts/run_vms.sh: don't source oe-init-build-env

--- scripts/run_vms.sh
+++ scripts/run_vms.sh
@@ -7,9 +7,7 @@ set -e
 
 . "$(dirname $0)"/common_variables.sh
 
-cd "$YOCTO_WORK_DIR"/poky
-
-source oe-init-build-env build
+cd "$YOCTO_WORK_DIR"/poky/build
 
 #runqemu qemux86-64 nographic slirp
 
