ee695050fd2a8ce49fbe50a3d3cfbd4363b80c6a
Maxim Karasev <mxkrsv@disroot.org>
2023-08-02 14:32:14 +0300

    scripts/run_vms.sh: forward ssh

--- scripts/run_vms.sh
+++ scripts/run_vms.sh
@@ -42,8 +42,8 @@ COMMON_OPTIONS_DEFAULT="-usb -device usb-tablet -usb -device usb-kbd \
 	-cpu IvyBridge -machine q35,i8042=off -smp 4 -m 256 \
 	-nographic -monitor null"
 
-VM1_OPTIONS_DEFAULT="-serial telnet::8000,server,nowait"
-VM2_OPTIONS_DEFAULT="-serial telnet::8001,server,nowait"
+VM1_OPTIONS_DEFAULT="-serial telnet::8000,server,nowait -net nic -net user,hostfwd=tcp::7000-:22"
+VM2_OPTIONS_DEFAULT="-serial telnet::8001,server,nowait -net nic -net user,hostfwd=tcp::7001-:22"
 
 if [ "$COMMON_OPTIONS_OVERRIDE" ]; then
 	COMMON_OPTIONS="$COMMON_OPTIONS_OVERRIDE"
