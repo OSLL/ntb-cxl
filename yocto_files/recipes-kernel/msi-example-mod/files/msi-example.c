b05bcf56573429e3be3f2f1a198c77a7c18f50f6
Maxim Karasev <mxkrsv@disroot.org>
2023-08-10 18:52:35 +0300

    msi-example-mod: implement msi irq handling

--- yocto_files/recipes-kernel/msi-example-mod/files/msi-example.c
+++ yocto_files/recipes-kernel/msi-example-mod/files/msi-example.c
@@ -1,16 +1,82 @@
 #include <linux/module.h>
+#include <linux/pci.h>
 
-static int __init hello_init(void)
+struct pci_device_id devid = {
+	PCI_DEVICE(0x1337, 0x0001)
+};
+
+static irqreturn_t msi_example_handle_irq(int n, void* data)
 {
-	pr_info("Hello World!\n");
+	pr_info("msi-example: got an interrupt\n");
+
 	return 0;
 }
 
-static void __exit hello_exit(void)
+static int msi_example_probe(struct pci_dev *dev, const struct pci_device_id *id)
 {
-	pr_info("Goodbye Cruel World!\n");
+	int err;
+
+	err = pci_enable_device(dev);
+	if (err)
+	{
+		pr_err("msi-example: pci_enable_device failed with code %d\n", err);
+		return 1;
+	}
+
+	err = pci_alloc_irq_vectors(dev, 1, 1, PCI_IRQ_MSI);
+	if (err < 0)
+	{
+		pr_err("msi-example: pci_alloc_irq_vectors failed with code %d\n", err);
+		return 2;
+	}
+
+	err = pci_request_irq(dev, 0, &msi_example_handle_irq, NULL, &devid, "msi-example-handler");
+	if (err)
+	{
+		pr_err("msi-example: pci_request_irq failed with code %d\n", err);
+		return 3;
+	}
+
+	pr_info("msi-example: probed a device\n");
+	return 0;
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+static void msi_example_remove(struct pci_dev *dev)
+{
+	pci_free_irq(dev, 0, NULL);
+
+	pci_free_irq_vectors(dev);
+
+	pr_info("msi-example: removed a device\n");
+}
+
+struct pci_driver drv = {
+	.name = "msi-example",
+	.id_table = &devid,
+	.probe = &msi_example_probe,
+	.remove = &msi_example_remove,
+};
+
+static int __init msi_example_init(void)
+{
+	int err = pci_register_driver(&drv);
+	if (err)
+	{
+		pr_err("msi-example: pci_register_driver failed with code %d\n", err);
+		return 1;
+	}
+
+	pr_info("msi-example: loaded\n");
+	return 0;
+}
+
+static void __exit msi_example_exit(void)
+{
+	pci_unregister_driver(&drv);
+
+	pr_info("msi-example: unloaded\n");
+}
+
+module_init(msi_example_init);
+module_exit(msi_example_exit);
 MODULE_LICENSE("GPL");
