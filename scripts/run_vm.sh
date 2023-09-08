38a4338b7b6f634f9e526671c42f83f644c678d5
Maxim Karasev <mxkrsv@disroot.org>
2023-09-08 21:54:29 +0300

    script/run_vm.sh: allow passing VM1_OPTIONS{,_OVERRIDE} to runqemu

--- scripts/run_vm.sh
+++ scripts/run_vm.sh
@@ -11,7 +11,7 @@ cd "$YOCTO_WORK_DIR"/poky
 
 source oe-init-build-env build
 
-runqemu qemux86-64 nographic slirp qemuparams="$COMMON_OPTIONS"
+runqemu qemux86-64 nographic slirp qemuparams="$COMMON_OPTIONS ${VM1_OPTIONS_OVERRIDE:-$VM1_OPTIONS}"
 
 set +x
 
